c088599d7845ea88b12268d1d4da6072d9cd7d15
crmplusoficina-star <crmplus.oficina@gmail.com>
2026-08-25 13:47:12 -0300

    feat: gate app by matricula session

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { SessionProvider } from './session';
 import './enhancements.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SessionProvider><App /></SessionProvider>
+  </React.StrictMode>
+);
