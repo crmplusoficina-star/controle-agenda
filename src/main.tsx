ab84835f14632f35fa2463e3090da20cb063f03a
crmplusoficina-star <crmplus.oficina@gmail.com>
2026-08-31 20:56:32 -0300

    Mount first-access tutorial overlay

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { SessionProvider } from './session';
 import { ArIAWidget } from './components/ArIAWidget';
+import { TutorialOverlay } from './components/TutorialOverlay';
 import './enhancements.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SessionProvider>
       <App />
+      <TutorialOverlay />
       <ArIAWidget />
     </SessionProvider>
   </React.StrictMode>
