e37bd5c27e04f060cbae61a370a0e5e08303cb16
crmplusoficina-star <crmplus.oficina@gmail.com>
2026-08-31 20:25:00 -0300

    Mount ArIA floating assistant sketch

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { SessionProvider } from './session';
+import { ArIAWidget } from './components/ArIAWidget';
 import './enhancements.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SessionProvider><App /></SessionProvider>
+    <SessionProvider>
+      <App />
+      <ArIAWidget />
+    </SessionProvider>
   </React.StrictMode>
 );
